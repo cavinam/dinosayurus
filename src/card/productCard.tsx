f69f2849c7b98e07b6e6923dda5e7cf50c67e6cf
cavinam <cavinaditya5@gmail.com>
2024-12-27 08:27:30 +0700

    Add Heigh and Width

--- src/card/productCard.tsx
+++ src/card/productCard.tsx
@@ -5,9 +5,9 @@ import { IProduct } from "@/data/prod";
 const ProductCard = ({ product }: { product: IProduct }) => {
   console.log(product);
   return (
-    <div className="flex flex-col items-center max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <div className="relative w-40 h-40 p-4">
-        <Image src={product.image} alt={product.name} fill />
+    <div className="flex flex-col items-center bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="relative w-[200px] h-[200px] p-4">
+        <Image src={product.image} alt={product.name} height={80} width={80} />
       </div>
       <div className="p-5">
         <h5 className="mt-4 text-center font-bold text-2xl">{product.name}</h5>
